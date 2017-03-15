8ef422177acea049b680a5944bbf1c1e810aad45
MONTES Yannick <montes.yannick@gmail.com>
2017-03-15 11:47:37 +0100

    Add adapter pattern and implement function already coded in Graph.java

--- src/main/java/graph/UndirectedGraph.java
+++ src/main/java/graph/UndirectedGraph.java
@@ -12,30 +12,42 @@ import java.util.Set;
  *
  * @author yoannlathuiliere
  */
-public class UndirectedGraph implements IUndirectedGraph{
-
-    public void addEdge(Node _node1, Node _node2) {
+public class UndirectedGraph implements IUndirectedGraph
+{
+    private Graph graph;
+    
+    public UndirectedGraph(Graph g)
+    {
+        this.graph = g;
+    }
+    public void addEdge(Node _node1, Node _node2) 
+    {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public boolean hasEdge(Node _node1, Node _node2) {
+    public boolean hasEdge(Node _node1, Node _node2) 
+    {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void addNode(Node _node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void addNode(Node _node) 
+    {
+        this.graph.addNode(_node);
     }
 
-    public Set<Node> getAllNodes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Set<Node> getAllNodes() 
+    {
+        return this.graph.getAllNodes();
     }
 
-    public int getNbNodes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getNbNodes() 
+    {
+        return this.graph.getNbNodes();
     }
 
-    public List<Node> getAdjNodes(Node _n) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Node> getAdjNodes(Node _n) 
+    {
+        return this.graph.getAdjNodes(_n);
     }
     
 }
