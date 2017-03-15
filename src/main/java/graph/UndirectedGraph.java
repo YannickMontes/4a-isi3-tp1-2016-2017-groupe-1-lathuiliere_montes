df6e4afac247bdc30c0ef9eafc15c189925d7030
YoannLTH <yoann.lathuiliere@gmail.com>
2017-03-15 11:10:25 +0100

    Add class

---
+++ src/main/java/graph/UndirectedGraph.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graph;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author yoannlathuiliere
+ */
+public class UndirectedGraph implements IUndirectedGraph{
+
+    public void addEdge(Node _node1, Node _node2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean hasEdge(Node _node1, Node _node2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addNode(Node _node) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Set<Node> getAllNodes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getNbNodes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Node> getAdjNodes(Node _n) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
